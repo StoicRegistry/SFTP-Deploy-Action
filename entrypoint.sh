d972d5c1501703987950bad85d3e305d6f653e27
stephandue <stephan.duechtel@gmail.com>
2024-03-14 01:17:52 +0100

    Update entrypoint.sh (added sync_directories function)

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,25 @@ if [ -z "$6" ]; then
    exit 1
 fi
 
+# Function to remove files in the remote path that do not exist in the local path
+sync_directories() {
+    local_path=$1
+    remote_path=$2
+    host=$3
+    user=$4
+    port=$5
+    password=$6
+
+    # List files in local directory
+    local_files=$(ls -1 "$local_path")
+
+    # Generate a command to list files in the remote directory, compare with local files, and remove the difference
+    remove_command=$(printf "ls -1 %s | grep -vFx -e %s | xargs -r rm -f" "$remote_path" "$local_files")
+
+    # Execute the command on the remote server
+    SSHPASS=$password sshpass -e ssh -o StrictHostKeyChecking=no -p $port $user@$host "$remove_command"
+}
+
 # use password
 if [ -z != ${10} ]; then
 	echo 'use sshpass'
@@ -33,6 +52,9 @@ if [ -z != ${10} ]; then
 	printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
 	#-o StrictHostKeyChecking=no avoid Host key verification failed.
 	SSHPASS=${10} sshpass -e sftp -oBatchMode=no -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no $1@$2
+
+	# Call the function to sync directories
+	sync_directories $5 $6 $2 $1 $3 ${10}
 
 	echo 'Deploy Success'
 
